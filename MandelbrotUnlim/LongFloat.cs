e8be4c29f16e9937ff995e8002bc8c20a83ac338
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-09-09 17:19:49 +0300

    develope long arithmetics

--- MandelbrotUnlim/LongFloat.cs
+++ MandelbrotUnlim/LongFloat.cs
@@ -124,6 +124,13 @@ namespace MandelbrotUnlim
             }
         }
 
+        public LongFloat()
+        {
+	        sign = 1;
+            digits = new List<int> { 0 };
+	        exponent = 1;
+        }
+
         public LongFloat(string s)
         {
             InitFromString(s);
@@ -142,6 +149,148 @@ namespace MandelbrotUnlim
         public static LongFloat FromDouble(double d)
         {
             return new LongFloat(d);
+        }
+
+        public static LongFloat operator-(LongFloat a) 
+        {
+            LongFloat result = new LongFloat();
+
+            int[] digs = new int[a.digits.Count];
+            a.digits.CopyTo(digs);
+
+            result.digits = new List<int>(digs);
+            result.exponent = a.exponent;
+            result.sign = -a.sign;
+
+	        return result;
+        }
+
+        public static LongFloat operator+(LongFloat a, LongFloat b)
+        {
+            if (a.sign == b.sign)
+            {
+                int exp1 = a.exponent;
+                int exp2 = b.exponent;
+                int exp = Math.Max(exp1, exp2);
+
+                List<int> d1 = new List<int>(a.digits);
+                List<int> d2 = new List<int>(b.digits);
+
+                while (exp1 != exp)
+                {
+                    d1.Insert(0, 0);
+                    exp1++;
+                }
+
+                while (exp2 != exp)
+                {
+                    d2.Insert(0, 0);
+                    exp2++;
+                }
+
+                int size = Math.Max(d1.Count, d2.Count);
+
+                while (d1.Count != size)
+                    d1.Add(0);
+
+                while (d2.Count != size)
+                    d2.Add(0);
+
+                int len = 1 + size;
+
+                LongFloat res = new LongFloat();                
+
+                int[] zeros = new int[len];
+                Array.Clear(zeros, 0, len);
+
+                res.digits = new List<int>(zeros);
+                res.sign = a.sign;
+
+                for (int i = 0; i < size; i++)
+                    res.digits[i + 1] = d1[i] + d2[i];
+
+                for (int i = len - 1; i > 0; i--)
+                {
+                    res.digits[i - 1] += res.digits[i] / 10;
+                    res.digits[i] %= 10;
+                }
+
+                res.exponent = exp + 1;
+                res.RemoveZeroes();
+
+                return res;
+            }
+
+            if (a.sign == -1)
+                return b - (-a);
+
+            return a - (-b);
+        }
+
+        public static LongFloat operator -(LongFloat a, LongFloat b)
+        {
+            /*if (a.sign == 1 && b.sign == 1)
+            {
+                bool cmp = *this > x;
+
+                long exp1 = cmp ? a.exponent : b.exponent;
+                long exp2 = cmp ? b.exponent : a.exponent;
+                long exp = Math.Max(exp1, exp2);
+
+                vector<int> d1(cmp? digits : x.digits);
+                vector<int> d2(cmp? x.digits : digits);
+
+                while (exp1 != exp)
+                {
+                    d1.insert(d1.begin(), 0);
+                    exp1++;
+                }
+
+                while (exp2 != exp)
+                {
+                    d2.insert(d2.begin(), 0);
+                    exp2++;
+                }
+
+                int size = max(d1.size(), d2.size());
+
+                while (d1.size() != size)
+                    d1.push_back(0);
+
+                while (d2.size() != size)
+                    d2.push_back(0);
+
+                int len = 1 + size;
+
+                LongFloat res = new LongFloat();
+
+                res.sign = cmp ? 1 : -1;
+                res.digits = vector<int>(len, 0);
+
+                for (size_t i = 0; i < size; i++)
+                    res.digits[i + 1] = d1[i] - d2[i];
+
+                for (size_t i = len - 1; i > 0; i--)
+                {
+                    if (res.digits[i] < 0)
+                    {
+                        res.digits[i] += 10;
+                        res.digits[i - 1]--;
+                    }
+                }
+
+                res.exponent = exp + 1;
+                res.removeZeroes();
+
+                return res;
+            }
+
+            if (sign == -1 && x.sign == -1)
+                return (-x) - (-(*this));
+
+            return *this + (-x);*/
+
+            return null;
         }
     }
 }
