6d47b4b54be7d0cf3fda8cc353a6ad1e08ea447b
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-09-09 11:28:33 +0300

    Fill GUI

--- MandelbrotUnlim/Form1.Designer.cs
+++ MandelbrotUnlim/Form1.Designer.cs
@@ -28,29 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblElapsedTime = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(84, 51);
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // lblElapsedTime
+            // 
+            this.lblElapsedTime.Location = new System.Drawing.Point(658, 157);
+            this.lblElapsedTime.Name = "lblElapsedTime";
+            this.lblElapsedTime.Size = new System.Drawing.Size(206, 20);
+            this.lblElapsedTime.TabIndex = 21;
+            this.lblElapsedTime.Text = "label4";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnZoomOut);
+            this.groupBox1.Controls.Add(this.btnZoomIn);
+            this.groupBox1.Controls.Add(this.btnLeft);
+            this.groupBox1.Controls.Add(this.btnUp);
+            this.groupBox1.Controls.Add(this.btnRight);
+            this.groupBox1.Controls.Add(this.btnDown);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(658, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 142);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Навигация";
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(109, 19);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(78, 31);
+            this.btnZoomOut.TabIndex = 17;
+            this.btnZoomOut.Text = "-";
+            this.btnZoomOut.UseVisualStyleBackColor = true;
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(25, 19);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(78, 31);
+            this.btnZoomIn.TabIndex = 15;
+            this.btnZoomIn.Text = "+";
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLeft.BackgroundImage")));
+            this.btnLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnLeft.Location = new System.Drawing.Point(25, 56);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(36, 36);
+            this.btnLeft.TabIndex = 5;
+            this.btnLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnUp.BackgroundImage")));
+            this.btnUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnUp.Location = new System.Drawing.Point(67, 56);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(36, 36);
+            this.btnUp.TabIndex = 6;
+            this.btnUp.UseVisualStyleBackColor = true;
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRight.BackgroundImage")));
+            this.btnRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRight.Location = new System.Drawing.Point(151, 56);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(36, 36);
+            this.btnRight.TabIndex = 7;
+            this.btnRight.UseVisualStyleBackColor = true;
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDown.BackgroundImage")));
+            this.btnDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnDown.Location = new System.Drawing.Point(109, 56);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(36, 36);
+            this.btnDown.TabIndex = 8;
+            this.btnDown.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Исходное положение";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(658, 618);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(206, 34);
+            this.btnClose.TabIndex = 22;
+            this.btnClose.Text = "Выход";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(781, 611);
+            this.ClientSize = new System.Drawing.Size(874, 661);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblElapsedTime);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Фрактал Мандельбота";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -58,6 +176,16 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblElapsedTime;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnZoomOut;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- MandelbrotUnlim/Form1.cs
+++ MandelbrotUnlim/Form1.cs
@@ -12,9 +12,138 @@ namespace MandelbrotUnlim
 {
     public partial class Form1 : Form
     {
+
+        #region memebers
+
+        private Bitmap _bitmap = null;
+        private byte[] _rgbValues = null;
+        private Palette _colors = null;
+
+        private const double cStep = 10;
+
+        private const double cXmin = -1.75;
+        private const double cXmax = 1.75;
+        private const double cYmin = -1.75;
+        private const double cYmax = 1.75;
+
+        private int _zoom = 0;
+        private int _timeSpan;
+
+        #endregion
+
+        #region private
+
+        Palette CreateColdPalette()
+        {
+            Palette palette = new Palette();
+
+            palette.AddBaseColor(Color.FromArgb(0, 0, 128));
+            palette.AddBaseColor(Color.FromArgb(0, 0, 192));
+            palette.AddBaseColor(Color.FromArgb(0, 255, 255));
+            palette.AddBaseColor(Color.FromArgb(0, 255, 0));
+
+            palette.AddBaseColor(Color.FromArgb(0, 128, 0));
+            palette.AddBaseColor(Color.FromArgb(128, 128, 0));
+            palette.AddBaseColor(Color.FromArgb(255, 255, 0));
+            palette.AddBaseColor(Color.FromArgb(255, 0, 0));
+
+            palette.CreatePalette();
+
+            return palette;
+        }
+
+        Bitmap CreateBackground(int width, int height)
+        {
+            return (width > 0 || height > 0) ? new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb) : null;
+        }
+
+        Bitmap GetFractal()
+        {
+            /*Rectangle rect = new Rectangle(0, 0, _bitmap.Width, _bitmap.Height);
+            System.Drawing.Imaging.BitmapData bmpData = _bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            int stride = bmpData.Stride;
+            int bytes = Math.Abs(stride) * _bitmap.Height;
+
+            FractalBitmap fbi = new FractalBitmap()
+            {
+                BitmapWidth = _bitmap.Width,
+                BitmapHeight = _bitmap.Height,
+                Xmin = _xmin,
+                Xmax = _xmax,
+                Ymin = _ymin,
+                Ymax = _ymax
+            };
+
+            _timeSpan = fbi.GetBitmap(Fractal, _colors.Colors, stride, _rgbValues);
+
+            System.Runtime.InteropServices.Marshal.Copy(_rgbValues, 0, bmpData.Scan0, bytes);
+            _bitmap.UnlockBits(bmpData);*/
+
+            return _bitmap;
+        }
+
+        void UpdateFractal()
+        {
+            pictureBox1.Image = GetFractal();
+            lblElapsedTime.Text = string.Format("Время построения: {0} мс", _timeSpan);
+        }
+
+        /*AbstractDynamicFractal Fractal
+        {
+            get
+            {
+                AbstractDynamicFractal fractal;
+
+                if (cmbFractal.SelectedIndex == 0)
+                {
+                    var a = 2.0 / (trackBar1.Maximum - trackBar1.Minimum) * (trackBar1.Value - trackBar1.Minimum) - 1;
+                    var b = 2.0 / (trackBar2.Maximum - trackBar2.Minimum) * (trackBar2.Value - trackBar2.Minimum) - 1;
+
+                    fractal = new JuliaFractal(new Complex(a, b));
+                }
+                else if (cmbFractal.SelectedIndex == 1)
+                {
+                    fractal = new MandelbrotFractal();
+                }
+                else
+                {
+                    fractal = new NewtonFractal((int)cmbRoots.Items[cmbRoots.SelectedIndex]);
+                }
+
+                return fractal;
+            }
+        }*/
+
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _bitmap = CreateBackground(pictureBox1.Width, pictureBox1.Height);
+
+            if (_bitmap != null)
+            {
+                Rectangle rect = new Rectangle(0, 0, _bitmap.Width, _bitmap.Height);
+                System.Drawing.Imaging.BitmapData bmpData = _bitmap.LockBits(rect,
+                System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                int stride = bmpData.Stride;
+                int bytes = Math.Abs(stride) * _bitmap.Height;
+                _rgbValues = new byte[bytes];
+                _bitmap.UnlockBits(bmpData);
+            }
+
+            UpdateFractal();
         }
     }
 }
