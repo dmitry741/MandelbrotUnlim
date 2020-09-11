46606747fefd286c5913b9ae24485ff19f14317f
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-09-11 11:39:07 +0300

    Add Mandelbrot's fractal class

---
+++ MandelbrotUnlim/AbstractDynamicFractal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandelbrotUnlim
+{
+    /// <summary>
+    /// Абстрактный класс представляющий фрактал.
+    /// </summary>
+    public abstract class AbstractDynamicFractal
+    {
+        /// <summary>
+        /// Максимальное количество итераций.
+        /// </summary>
+        public int MaxIterationCount { get; set; }
+
+        /// <summary>
+        /// Точка с которой начинается итеративный процесс.  
+        /// </summary>
+        public Complex Start { get; set; }
+
+        #region === abstarct ===
+
+        /// <summary>
+        /// Получение следующей точки.
+        /// </summary>
+        /// <param name="z">Исходная точка.</param>
+        /// <returns>Новая точка.</returns>
+        protected abstract Complex NextIteration(Complex z);
+
+        /// <summary>
+        /// Проверка условия завершения итеративного процесса.
+        /// </summary>
+        /// <param name="z">Точка на плоскости.</param>
+        /// <returns>True если следует завершить итеративный процесс, False в противоположном случае.</returns>
+        protected abstract bool Check(Complex z);
+
+        public abstract AbstractDynamicFractal Copy();
+
+        #endregion
+
+        /// <summary>
+        /// Получение индекса, на котором был прерван итеративный процесс.
+        /// </summary>
+        /// <returns>Индекс итерации.</returns>
+        public int GetIteration()
+        {
+            Complex z1 = Start;
+            int index = -1;
+
+            for (int i = 0; i < MaxIterationCount; i++)
+            {
+                Complex z2 = NextIteration(z1);
+
+                if (Check(z2))
+                {
+                    index = i;
+                    break;
+                }
+
+                z1 = z2;
+            }
+
+            return index;
+        }
+    }
+}
---
+++ MandelbrotUnlim/MandelbrotFractal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LongFloatLib;
+
+namespace MandelbrotUnlim
+{
+    /// <summary>
+    ///  Фрактал Мандельброта.
+    /// </summary>
+    public class MandelbrotFractal : AbstractDynamicFractal
+    {
+        readonly LongFloat cTolerance = LongFloat.FromDouble(4);
+
+        protected override Complex NextIteration(Complex z)
+        {
+            return z * z + Start;
+        }
+
+        protected override bool Check(Complex z)
+        {
+            return z.ModuleInSquare > cTolerance;
+        }
+
+        public override AbstractDynamicFractal Copy() { return new MandelbrotFractal(); }
+    }
+}
